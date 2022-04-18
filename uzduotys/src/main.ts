a7056c48a9f1c39c3778cdedf08d2328b9841a7d
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-04-18 13:31:33 +0300

    content update

--- uzduotys/src/main.ts
+++ uzduotys/src/main.ts
@@ -1,3 +1,54 @@
+/*
+  1. Sukurkitę sąrašo mazgo struktūrą ListNode, bet kokiam duomenų tipui
+
+  2. Sukurkite sąrašo klasę List.
+    * Klasė turi privačias turėti savybes
+      * head -> nuoroda į sąrašo pirmajį mazgą
+      * taip -> nuoroda į sąrašo paskutinijė mazgą
+    * Sukurkite konstruktorių, jog jis galėtų priimti pirmu parametru perduotą ListNode elementą ir priskirti
+    jį savybėms List.head ir List.tail
+    Konstruktoriaus ListNode tipo parametras neprivalomas. Neperdavus parametro, List klasės objekto savybės tail ir head turi būti null
+
+  3. Sukurkite metodą pridėti sąrašo elementui į sąrašo priekį.
+    Payzdys:
+      esamas sąrašas:
+        head ↓         ↓ tail
+             a -> b -> c
+      naujas elementas
+        d
+      rezultatas
+        head ↓              ↓ tail
+             d -> a -> b -> c
+
+    Pridedant elementą įsitikintite, kad atnaujinate List.head ir List.tail savybes
+    * Po sąrašo List atnaujinimo:
+    List.head turi būti pirmasis mazgas(ListNode)
+    List.tail turi būti paskutinis mazgas(ListNode)
+    * Nepamirškite įvertinti atvejo, kuomet sąrašas List elementų neturi.
+
+  4. Sukurkite metodą pridėti sąrašo elementui į sąrašo galą.
+    Payzdys:
+      esamas sąrašas:
+        head ↓         ↓ tail
+             a -> b -> c
+      naujas elementas
+        d
+      rezultatas
+        head ↓              ↓ tail
+             a -> b -> c -> d
+
+    Pridedant elementą įsitikintite, kad atnaujinate List.head ir List.tail savybes
+    * Po sąrašo List atnaujinimo:
+    List.head turi būti pirmasis mazgas(ListNode)
+    List.tail turi būti paskutinis mazgas(ListNode)
+    * Nepamirškite įvertinti atvejo, kuomet sąrašas List elementų neturi.
+
+  5. Sukurkite metodą List.forEach klasėje List, kuris vykdytų parametru perduotą funkciją - callback
+  su kiekvieno mazgo reikšme pradedant List.head ir baigiant List.tail
+    * ForEachCallback tipas: (value: T) => void
+    * List.forEach tipas: ( callback: ForEachCallback) => void)
+*/
+
 // ↓↓↓ Tipai ↓↓↓
 
 // ↑↑↑ Tipai ↑↑↑
