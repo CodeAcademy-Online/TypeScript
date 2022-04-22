7a42824212ec6471246f8d89419fdf75b9d53781
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-04-22 06:52:15 +0300

    init files

---
+++ atsakymas copy/src/helpers/stingify-object.ts
@@ -0,0 +1,17 @@
+type StringifyObjectProps<Type extends Object> = {
+  [Key in keyof Type]: string
+};
+
+const stringifyProps = <Type extends Object>(object: Type): StringifyObjectProps<Type> => {
+  const objectLikeArray = Object.entries(object);
+
+  const objectWithPropsStringified = objectLikeArray
+    .reduce<Partial<StringifyObjectProps<Type>>>((prevObj, [key, value]) => ({
+      ...prevObj,
+      [key]: String(value),
+    }), {});
+
+  return objectWithPropsStringified as StringifyObjectProps<Type>;
+};
+
+export default stringifyProps;
