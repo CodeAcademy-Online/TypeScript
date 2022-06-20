ba8e70f7070becb358959926446ea0c306f7921a
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-06-20 07:44:52 +0300

    4 pamoka: pradiniai failai

---
+++ 4 - Išplėstiniai tipai/užduotys pamokos darbui/atsakymai/src/main.ts
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+/* eslint-disable no-inner-declarations */
+/* eslint-disable no-lone-blocks */
+
+
+/*
+  Unions
+  Tuples
+  Assertions
+  String Literals
+  Enums
+*/
+
+console.groupCollapsed('');
+{
+
+}
+console.groupEnd();
